e131604456723f2f4f01239fc5f4cc1f92e2d44b
Saw Wera Kyaw Kyaw <sk046@bucknell.edu>
2025-03-11 21:08:41 -0400

    Add user birthday in schema

--- lib/todo/accounts/user.ex
+++ lib/todo/accounts/user.ex
@@ -6,7 +6,6 @@ defmodule Todo.Accounts.User do
   schema "users" do
     field :email, :string
     field :name, :string
-    field :birthday, :date
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
@@ -40,9 +39,16 @@ defmodule Todo.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :name])
     |> validate_email(opts)
     |> validate_password(opts)
+    |> validate_name()
+  end
+
+  defp validate_name(changeset) do
+    changeset
+    |> validate_required([:name])
+    |> validate_length(:name, min: 2, max: 100)
   end
 
   defp validate_email(changeset, opts) do
